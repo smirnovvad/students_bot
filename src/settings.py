2ec9ce838d31bd38009f4a7cd9a86d714d808437
Vadim Smirnov <smirnovvad7@gmail.com>
2018-05-04 03:24:54 +0300

    Some syntax refactor

--- src/settings.py
+++ src/settings.py
@@ -1,10 +1,5 @@
-
-
-from dotenv import load_dotenv
-
-from pathlib import Path  # python3 only
-env_path = Path('.') / '.env'
-load_dotenv(verbose=True, dotenv_path=env_path)
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv(), verbose=True)
 import os
 API_URL=os.getenv("API_URL")
 TELEGRAM_TOKEN=os.getenv("TELEGRAM_TOKEN")
